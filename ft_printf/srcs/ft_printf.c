0ade545b70589afdb4880ba6a2e7084edbc18842
Rafael Filipe Tavares Cardoso <ratavare@c1r16s1.42lisboa.com>
2022-11-30 20:25:05 +0000

    30/11/2022

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ratavare <ratavare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 17:55:34 by ratavare          #+#    #+#             */
-/*   Updated: 2022/11/29 19:12:54 by ratavare         ###   ########.fr       */
+/*   Updated: 2022/11/30 20:23:06 by ratavare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ int	ft_printf(const char *format, ...)
 			len += ft_print_char(format[i]);
 		i++;
 	}
+	va_end(args);
 	return (len);
 }
 
@@ -41,14 +42,14 @@ int	ft_print_args(const char format, va_list args)
 		return (ft_print_char(va_arg(args, int)));
 	if (format == 's')
 		return (ft_print_string(va_arg(args, char *)));
-	//if (format == 'p')
-	//	return (ft_print_uhex(va_arg(args, unsigned long)));
+	if (format == 'p')
+		return (ft_print_uhex(va_arg(args, unsigned long)));
 	if (format == 'd' || format == 'i')
 		return (ft_print_nbr(va_arg(args, int)));
 	if (format == 'u')
 		return (ft_print_unbr(va_arg(args, unsigned long)));
 	if (format == 'x' || format == 'X')
-		return (ft_print_hex(va_arg(args, int), format));
+		return (ft_print_hex(va_arg(args, unsigned int), format));
 	if (format == '%')
 		return (ft_print_char('%'));
 	return (0);
@@ -58,4 +59,25 @@ int	ft_print_char(const char format)
 {
 	write(1, &format, 1);
 	return (1);
+}
+
+int	hexcount(unsigned long long x)
+{
+	int	ret;
+
+	ret = 0;
+	while (x != 0)
+	{
+		ret++;
+		x = x / 16;
+	}
+	return (ret);
+}
+
+int	ft_print_uhex(unsigned long n)
+{
+	if (n == 0)
+		return (ft_print_string("(nil)"));
+	write (1, "0x", 2);
+	return (ft_puthex(n));
 }
--- ft_printf/srcs/ft_printf_utils.c
+++ ft_printf/srcs/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: ratavare <ratavare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:47:03 by ratavare          #+#    #+#             */
-/*   Updated: 2022/11/29 19:14:02 by ratavare         ###   ########.fr       */
+/*   Updated: 2022/11/30 20:21:49 by ratavare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	ft_print_string(const char *format)
 	int	i;
 
 	i = 0;
+	if (format == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
 	while (format[i])
 		ft_print_char(format[i++]);
 	return (i);
@@ -24,27 +29,12 @@ int	ft_print_string(const char *format)
 
 int	ft_print_nbr(int n)
 {
-	static int	ret;
+	int		ret;
+	char	*str;
 
-	ret = 0;
-	if (n == -2147483648)
-	{
-		ft_print_char('-');
-		write(1, "2147483648", 10);
-		return (11);
-	}
-	if (n < 0)
-	{
-		ret += ft_print_char('-');
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		ft_print_nbr(n / 10);
-		n %= 10;
-	}
-	if (n < 10)
-		ret += ft_print_char(n + 48);
+	str = ft_itoa(n);
+	ret = ft_print_string(str);
+	free (str);
 	return (ret);
 }
 
@@ -63,26 +53,37 @@ int	ft_print_unbr(unsigned int n)
 	return (ret);
 }
 
-int	ft_print_hex(int n, const char format)
+int	ft_print_hex(unsigned long n, const char format)
 {
-	int		ret;
-	char	*str;
-
-	str = ft_itoa(n);
-	ret = ft_strlen(str);
+	if (n == 0)
+		return (ft_print_char('0'));
 	if (n > 15)
 	{
 		ft_print_hex(n / 16, format);
 		ft_print_hex(n % 16, format);
 	}
-	if (n < 15 && n > 9)
+	if (n <= 9)
+		ft_print_char(n + 48);
+	else if (n < 16 && n > 9)
 	{
 		if (format == 'x')
 			ft_print_char(n + 87);
 		if (format == 'X')
 			ft_print_char(n + 55);
 	}
-	if (n < 9)
-		ft_print_nbr(n);
-	return (ret);
+	return (hexcount(n));
+}
+
+int	ft_puthex(unsigned long n)
+{
+	if (n > 15)
+	{
+		ft_puthex(n / 16);
+		ft_puthex(n % 16);
+	}
+	if (n <= 9)
+		ft_print_char(n + 48);
+	if (n < 16 && n > 9)
+		ft_print_char(n + 87);
+	return (hexcount(n) + 2);
 }
